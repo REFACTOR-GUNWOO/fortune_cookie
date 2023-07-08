b5b48bbd21c75c5ad42045dc9254f4c1b3ff658a
김건우B <gunwoo.kim@toss.im>
2023-07-08 13:54:32 +0900

    하단 탭바 스타일 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -79,44 +80,36 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child:  
-        SingleChildScrollView (child :Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '아침 시간대의 운세를 뽑아보세요',
-            ),
-            Container(
-              height: 200,
-              color: Colors.red,
-              margin: EdgeInsets.symmetric(horizontal: 100, vertical: 100),
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  FortuneHistory(label:"아침"),
-                  FortuneHistory(label:"점심"),
-                  FortuneHistory(label:"저녁"),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                '아침 시간대의 운세를 뽑아보세요',
               ),
-            ),
-          ],
-        ),
+              Container(
+                height: 200,
+                color: Colors.red,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 100),
+              ),
+              const FortuneHistoryContainer()
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -129,30 +122,28 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 const fortuneStringList = [
-    "곧 당신의 삶에 큰 기회가 찾아올 것입니다. 준비되어 있으세요.",
-    "어제의 실패는 내일의 성공을 위한 단서입니다. 오늘 더 나은 선택을 해보세요.",
-    "변화는 불가피한 것입니다. 새로운 상황에 적응하는 능력을 키워보세요.",
-    "힘들고 어려운 시기가 있더라도, 항상 희망을 잃지 마세요. 믿을 수 있는 미래가 기다리고 있습니다.",
-    "어떤 도전에 직면하더라도, 두려워하지 마세요. 당신은 그것을 이길 수 있는 힘을 갖고 있습니다.",
-    "주변 사람들과의 협력은 중요합니다. 서로에게 도움을 주고받을 수 있는 기회를 찾아보세요.",
-    "목표를 설정하고 꾸준히 노력하는 것은 성공의 핵심입니다. 포기하지 마세요.",
-    "어떤 어려움에 직면하더라도 자신의 내면에 힘을 찾아보세요. 그곳에 해답이 있을 것입니다.",
-    "삶은 여행입니다. 여러 경험을 쌓으며 자신을 발전시키세요.",
-    "가장 큰 성공은 자신을 인정하고 사랑하는 데서 찾을 수 있습니다. 자기 자신에게 자신감을 갖으세요."
+  "곧 당신의 삶에 큰 기회가 찾아올 것입니다. 준비되어 있으세요.",
+  "어제의 실패는 내일의 성공을 위한 단서입니다. 오늘 더 나은 선택을 해보세요.",
+  "변화는 불가피한 것입니다. 새로운 상황에 적응하는 능력을 키워보세요.",
+  "힘들고 어려운 시기가 있더라도, 항상 희망을 잃지 마세요. 믿을 수 있는 미래가 기다리고 있습니다.",
+  "어떤 도전에 직면하더라도, 두려워하지 마세요. 당신은 그것을 이길 수 있는 힘을 갖고 있습니다.",
+  "주변 사람들과의 협력은 중요합니다. 서로에게 도움을 주고받을 수 있는 기회를 찾아보세요.",
+  "목표를 설정하고 꾸준히 노력하는 것은 성공의 핵심입니다. 포기하지 마세요.",
+  "어떤 어려움에 직면하더라도 자신의 내면에 힘을 찾아보세요. 그곳에 해답이 있을 것입니다.",
+  "삶은 여행입니다. 여러 경험을 쌓으며 자신을 발전시키세요.",
+  "가장 큰 성공은 자신을 인정하고 사랑하는 데서 찾을 수 있습니다. 자기 자신에게 자신감을 갖으세요."
 ];
-
 
 class FortuneHistory extends StatelessWidget {
   final String label;
 
-  FortuneHistory({required this.label});
+  const FortuneHistory({Key? key, required this.label}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return                   
-      Container(
-      padding: EdgeInsets.all(5.0),
-      decoration: BoxDecoration(
+    return Container(
+      padding: const EdgeInsets.all(5.0),
+      decoration: const BoxDecoration(
         border: Border(
           bottom: BorderSide(
             color: Colors.grey,
@@ -165,28 +156,105 @@ class FortuneHistory extends StatelessWidget {
         children: [
           Text(
             label,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 15.0,
               fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(width: 8.0),
-                Flexible(
-          child: RichText(
-        overflow: TextOverflow.ellipsis,
-        maxLines: 5,
-        strutStyle: StrutStyle(fontSize: 16.0),
-        text: TextSpan(
-            text:            fortuneStringList[0],
-
-            style: TextStyle(
-                color: Colors.black,
-                height: 1.4,
-                fontSize: 16.0,
-                fontFamily: 'NanumSquareRegular')),
-         )),
+          const SizedBox(width: 8.0),
+          Flexible(
+              child: RichText(
+            overflow: TextOverflow.ellipsis,
+            maxLines: 5,
+            strutStyle: const StrutStyle(fontSize: 16.0),
+            text: TextSpan(
+                text: fortuneStringList[0],
+                style: const TextStyle(
+                    color: Colors.black,
+                    height: 1.4,
+                    fontSize: 16.0,
+                    fontFamily: 'NanumSquareRegular')),
+          )),
         ],
       ),
     );
+  }
+}
+
+class FortuneHistoryContainer extends StatelessWidget {
+  const FortuneHistoryContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: const EdgeInsets.symmetric(horizontal: 24.0),
+        shape: RoundedRectangleBorder(
+          //모서리를 둥글게 하기 위해 사용
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        elevation: 4.0, //그림자 깊이
+        child: const Row(children: [
+          Flexible(
+              child: FortuneHistoryCard(
+                  checked: true, label: '아침', timeString: '오전 6~9시'),
+              fit: FlexFit.tight),
+          Flexible(
+              child: FortuneHistoryCard(
+                  checked: false, label: '점심', timeString: '오전 11~2시'),
+              fit: FlexFit.tight),
+          Flexible(
+              child: FortuneHistoryCard(
+                  checked: false, label: '저녁', timeString: '오후 6~9시'),
+              fit: FlexFit.tight),
+        ], mainAxisAlignment: MainAxisAlignment.spaceBetween));
+  }
+}
+
+class FortuneHistoryCard extends StatelessWidget {
+  final String label;
+  final String timeString;
+  final bool checked;
+
+  Color cardColor() {
+    if (checked) {
+      return const Color.fromARGB(255, 243, 242, 237);
+    } else {
+      return Colors.transparent;
+    }
+  }
+
+  const FortuneHistoryCard(
+      {Key? key,
+      required this.label,
+      required this.timeString,
+      required this.checked})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 0,
+        margin: const EdgeInsets.all(8.0),
+        color: cardColor(),
+        shape: RoundedRectangleBorder(
+          //모서리를 둥글게 하기 위해 사용
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Container(
+            margin: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SvgPicture.asset('assets/icons/fortune_cookie.svg',
+                    width: 60, height: 60),
+                Text(
+                  label,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w800, fontSize: 24),
+                ),
+                Text(timeString,
+                    style: const TextStyle(
+                        fontSize: 14, color: Color.fromARGB(255, 94, 92, 85)))
+              ],
+            )));
   }
 }
